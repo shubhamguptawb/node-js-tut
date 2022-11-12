be3fd1bee802c430b78a339f48c15190b8167088
= <--replace-all>
2022-11-12 11:27:50 +0530

    response from server

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const http = require("http"); //look for a global file http
 
 function reqListener(req, res) {
-  console.log(req);
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My Fi rst Page</title> </head>");
+  res.write("<body><h1>Hello from my first Node js server </h1></body>");
+  res.write("</html >");
+  res.end();
 }
 
 //tell node js to listen this function for every single request
