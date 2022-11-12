e4d1d9a3a7f8f1f92e9180be62790cd376070366
= <--replace-all>
2022-11-12 16:50:10 +0530

    express js and middleware

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const http = require("http"); //look for a global file http
-const routes = require("./routes");
-const fs = require("fs");
+const express = require("express");
+//express execute a construction and create a object
+const app = express();
+//use allows us to use a middleware accepts 3 arguments req, res , next
+app.use((req, res, next) => {
+  console.log("In the middleware");
+  next();
+});
 
-const server = http.createServer(routes);
+app.use((req, res, next) => {
+  console.log("In another Middleware");
+});
+
+const server = http.createServer(app);
 
 server.listen(3000);
+
+//server response
