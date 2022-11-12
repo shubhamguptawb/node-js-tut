cbc7ba2c30001bfae30d6ddc5f88c05a1fbc55f6
= <--replace-all>
2022-11-12 11:36:23 +0530

    mulltiple pages

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
 const http = require("http"); //look for a global file http
 
 function reqListener(req, res) {
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>My Fi rst Page</title> </head>");
-  res.write("<body><h1>Hello from my first Node js server </h1></body>");
-  res.write("</html >");
-  res.end();
+  const url = req.url;
+  if (url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>My Fi rst Page</title> </head>");
+    res.write("<body><h1>Hello from my first Node js server </h1></body>");
+    res.write("</html >");
+    res.end();
+  } else if (url === "/test") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>My Fi rst Page</title> </head>");
+    res.write("<body><h1>Test Page </h1></body>");
+    res.write("</html >");
+    res.end();
+  } else {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>My Fi rst Page</title> </head>");
+    res.write("<body>Error 404</body>");
+    res.write("</html >");
+    res.end();
+  }
 }
 
 //tell node js to listen this function for every single request
