ff3871a23b47364de515a3d37b57995d9499cf06
= <--replace-all>
2022-11-12 17:20:07 +0530

    request in express

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
-const http = require("http"); //look for a global file http
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 //add slash for path
-
+//do not parse file , json etc only string
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/add-product", (req, res, next) => {
   //will send automatic header
-  res.send("<h1>Add Product Page</h1>");
+
+  res.send(
+    "<form action = '/product' method = 'POST'><input type = 'text' name = 'title' / ><button type = 'submit'>Add Product</button></form>"
+  );
 });
 
+app.use("/product", (req, res, next) => {
+  //request to don't parse body by itself
+  console.log(req.body, "body");
+  res.redirect("/");
+});
 app.use("/", (req, res, next) => {
   res.send("<h1>Hello World</h1>");
 });
