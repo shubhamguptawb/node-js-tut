677b4ad418518ed3a1bcc850623dd3e3ef036a16
= <--replace-all>
2022-11-12 12:26:40 +0530

    get data from client

--- server.js
+++ server.js
@@ -1,28 +1,35 @@
 const http = require("http"); //look for a global file http
-
+const fs = require("fs");
 function reqListener(req, res) {
   const url = req.url;
+  const method = req.method;
   if (url === "/") {
-    res.setHeader("Content-Type", "text/html");
     res.write("<html>");
-    res.write("<head><title>My Fi rst Page</title> </head>");
-    res.write("<body><h1>Hello from my first Node js server </h1></body>");
+    res.write("<head><title>Enter Message </title> </head>");
+    res.write(
+      '<body><form action= "/message" method="POST"><input type = "text" name = "message" ><button type = "submit">Submit</input></body>'
+    );
     res.write("</html >");
     res.end();
-  } else if (url === "/test") {
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write("<head><title>My Fi rst Page</title> </head>");
-    res.write("<body><h1>Test Page </h1></body>");
-    res.write("</html >");
-    res.end();
-  } else {
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write("<head><title>My Fi rst Page</title> </head>");
-    res.write("<body>Error 404</body>");
-    res.write("</html >");
-    res.end();
+  } else if (url === "/message" && method === "POST") {
+    //on allows to listen message because it is a buffer chunk
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk, "chunk");
+      //  <Buffer 6d 65 73 73 61 67 65 3d 6d 65 73 73 61 67 65> chunk
+      body.push(chunk);
+    });
+    //done parsing the requests
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      console.log(parsedBody, "parsed");
+      // message=message parsed
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("mesage.txt", message);
+    });
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
   }
 }
 
