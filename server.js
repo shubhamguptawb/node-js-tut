d67e42ac96c55e9f6a688bcea775f1101e4425e4
= <--replace-all>
2022-11-12 17:23:25 +0530

    app.get app.post

--- server.js
+++ server.js
@@ -2,23 +2,25 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 
-//add slash for path
-//do not parse file , json etc only string
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use("/add-product", (req, res, next) => {
+//app.use will trigger for every request
+//we can use app.get app.post instead for this
+
+//get request
+app.get("/add-product", (req, res, next) => {
   //will send automatic header
 
   res.send(
     "<form action = '/product' method = 'POST'><input type = 'text' name = 'title' / ><button type = 'submit'>Add Product</button></form>"
   );
 });
-
-app.use("/product", (req, res, next) => {
+//product page is post api
+app.post("/product", (req, res, next) => {
   //request to don't parse body by itself
   console.log(req.body, "body");
   res.redirect("/");
 });
-app.use("/", (req, res, next) => {
+app.get("/", (req, res, next) => {
   res.send("<h1>Hello World</h1>");
 });
 
