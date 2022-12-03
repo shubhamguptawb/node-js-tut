72005d8a108c954b503f6b5b1f91aac6b25f20bb
= <--replace-all>
2022-12-03 18:12:35 +0530

    internal css

--- routes/shop.js
+++ routes/shop.js
@@ -8,7 +8,7 @@ const router = express.Router();
 //send html files
 //we need to put project location for file not the os location so path module will be used0000
 router.get("/", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "../", "views", "shop.html"));
+  res.sendFile(path.join(rootDir, "views", "shop.html"));
 });
 //dirname will refer to location  of directory we  are in  i.e. address of routes folder  '//join is used because linus use \ window use /
 module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+const rootDir = require("./util/path");
 
 //router object becomes as a middleware
 const adminRoutes = require("./routes/admin");
@@ -16,6 +18,6 @@ app.use("/admin", adminRoutes);
 
 app.use(shopRoute);
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page not Found</h1>");
+  res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
 });
 app.listen(3000);
