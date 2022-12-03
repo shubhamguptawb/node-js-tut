f6d6edf0359c9af128e024d6b85d2b6cb597e3a9
= <--replace-all>
2022-12-03 17:06:54 +0530

    sending html file

--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,12 @@
+const path = require("path");
 const express = require("express");
 
 const router = express.Router();
 
+//send html files
+//we need to put project location for file not the os location so path module will be used
 router.get("/", (req, res, next) => {
-  res.send("<h1>Hello World</h1>");
+  res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
 });
-
+//dirname will refer to location  of directory we  are in  i.e. address of routes folder  '//join is used because linus use \ window use /
 module.exports = router;
