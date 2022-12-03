40630d53e0dc925617030402f3a88565add062d3
= <--replace-all>
2022-12-03 16:43:29 +0530

    routes in express

--- server.js
+++ server.js
@@ -1,28 +1,21 @@
 const express = require("express");
 const app = express();
+
+//router object becomes as a middleware
+const adminRoutes = require("./routes/admin");
+
+const shopRoute = require("./routes/shop");
+
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-//app.use will trigger for every request
-//we can use app.get app.post instead for this
 
-//get request
-app.get("/add-product", (req, res, next) => {
-  //will send automatic header
+//order of routes matter
+//only accept request starting with admin
+app.use("/admin", adminRoutes);
 
-  res.send(
-    "<form action = '/product' method = 'POST'><input type = 'text' name = 'title' / ><button type = 'submit'>Add Product</button></form>"
-  );
+app.use(shopRoute);
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not Found</h1>");
 });
-//product page is post api
-app.post("/product", (req, res, next) => {
-  //request to don't parse body by itself
-  console.log(req.body, "body");
-  res.redirect("/");
-});
-app.get("/", (req, res, next) => {
-  res.send("<h1>Hello World</h1>");
-});
-
-//previous two will be done automatically
 app.listen(3000);
