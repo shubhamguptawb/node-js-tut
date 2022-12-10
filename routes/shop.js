b6054f808c3c3c7945bd90949a1508454edc3642
= <--replace-all>
2022-12-10 18:08:14 +0530

    feat

--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,19 @@
-const express = require("express");
+const path = require('path');
 
-const productController = require("../controllers/products");
+const express = require('express');
+
+const shopController = require('../controllers/shop');
 
 const router = express.Router();
 
-router.get("/", productController.getProducts);
+router.get('/', shopController.getIndex);
+
+router.get('/products', shopController.getProducts);
+
+router.get('/cart', shopController.getCart);
+
+router.get('/orders', shopController.getOrders);
+
+router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
--- util/path.js
+++ util/path.js
@@ -1,4 +1,3 @@
-const path = require("path");
+const path = require('path');
 
-//gives the path to the directory where file is running the server
 module.exports = path.dirname(process.mainModule.filename);
