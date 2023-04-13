a0e446093f122d7428c103051b29bf886428e3a6
shubham-wb <scmotorsofficial@gmail.com>
2023-04-13 08:05:42 +0530

    feat : add product in mongo

--- util/database.js
+++ util/database.js
@@ -2,16 +2,29 @@
 
 const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
+let _db;
 
 const mongoConnect = (callback) => {
   MongoClient.connect(
-    "mongodb+srv://scmotorsofficial:AAGR2vWjNkHYQqkN@cluster0.cujoghp.mongodb.net/?retryWrites=true&w=majority"
+    "mongodb+srv://scmotorsofficial:AAGR2vWjNkHYQqkN@cluster0.cujoghp.mongodb.net/shop?retryWrites=true&w=majority"
   )
     .then((client) => {
       console.log("Connected");
-      callback(client);
+      _db = client.db();
+      callback();
     })
-    .catch((err) => console.log(err));
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
 };
 
-module.exports = mongoConnect;
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw "No Database Found";
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
