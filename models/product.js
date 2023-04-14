69b99a5277d24b6acab5c230445615c0a6a060bd
shubham-wb <scmotorsofficial@gmail.com>
2023-04-14 08:47:30 +0530

    feat:edit product mongo

--- models/product.js
+++ models/product.js
@@ -1,19 +1,27 @@
 const mongodb = require("mongodb");
 const getDb = require("../util/database").getDb;
 class Product {
-  constructor(title, price, description, imageUrl) {
+  constructor(title, price, description, imageUrl, id) {
     this.title = title;
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
+    this._id = id;
   }
   save() {
     const db = getDb();
-    return db
-      .collection("products")
-      .insertOne(this)
-      .then((result) => console.log(result))
-      .catch((err) => console.log(err));
+    let dbOp = db;
+    if (this._id) {
+      return dbOp
+        .collection("products")
+        .updateOne({ _id: new mongodb.ObjectId(this._id) }, { $set: this });
+    } else {
+      return dbOp
+        .collection("products")
+        .insertOne(this)
+        .then((result) => console.log(result))
+        .catch((err) => console.log(err));
+    }
   }
   //find gives a cursor only do not return a promise i.e. it gives only the amount of data that is required as there may be a million of data that cant be given in a go
 
@@ -38,7 +46,6 @@ class Product {
       .find({ _id: new mongodb.ObjectId(prodId) })
       .next()
       .then((product) => {
-        console.log(product);
         return product;
       })
       .catch((err) => {
