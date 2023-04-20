1a53eb6f670ae1e9298b3ea38d17096e84f3f197
shubham-wb <scmotorsofficial@gmail.com>
2023-04-20 06:48:58 +0530

    feat:session in db

--- app.js
+++ app.js
@@ -4,24 +4,32 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const session = require("express-session");
+
+const MongoDBStore = require("connect-mongodb-session")(session);
+
 const errorController = require("./controllers/error");
 const User = require("./models/user");
 
 const app = express();
-
+const store = new MongoDBStore({
+  uri: "mongodb+srv://scmotorsofficial:BBg38tYMYmFZ3dEF@cluster0.xzu3jyy.mongodb.net/test?retryWrites=true&w=majority",
+  collection: "sessions",
+});
 app.set("view engine", "ejs");
 app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const authRoutes = require("./routes/auth");
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(
   session({
-    secret: "EddfjER43647JHh!@2", //hash,
+    secret: "my secret",
     resave: false,
     saveUninitialized: false,
+    store: store,
   })
 );
 
@@ -37,18 +45,19 @@ app.use((req, res, next) => {
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
+
 app.use(errorController.get404);
 
 mongoose
   .connect(
-    "mongodb+srv://scmotorsofficial:AAGR2vWjNkHYQqkN@cluster0.cujoghp.mongodb.net/?retryWrites=true&w=majority"
+    "mongodb+srv://scmotorsofficial:BBg38tYMYmFZ3dEF@cluster0.xzu3jyy.mongodb.net/test?retryWrites=true&w=majority"
   )
   .then((result) => {
     User.findOne().then((user) => {
       if (!user) {
         const user = new User({
-          name: "Max",
-          email: "max@test.com",
+          name: "Shubham",
+          email: "shubham@gmail.com",
           cart: {
             items: [],
           },
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,18 @@
 exports.getLogin = (req, res, next) => {
-  const isLoggedIn = req.get("Cookie").split("=")[1];
-  console.log(isLoggedIn);
-  res.render("auth/login", {
-    path: "/login",
-    pageTitle: "login",
-    isAuthenticated: isLoggedIn,
+  //   const isLoggedIn = req
+  //     .get('Cookie')
+  //     .split(';')[1]
+  //     .trim()
+  //     .split('=')[1] === 'true';
+  console.log(req.session.isLoggedIn);
+  res.render('auth/login', {
+    path: '/login',
+    pageTitle: 'Login',
+    isAuthenticated: false
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  res.setHeader("Set-Cookie", "loggedIn=true;Max-Age=10");
-  res.redirect("/");
+  req.session.isLoggedIn = true;
+  res.redirect('/');
 };
